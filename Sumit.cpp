f6ca3da41d439a360369bfb9686463017741c393
bablucs12 <43750509+bablucs12@users.noreply.github.com>
2020-10-17 14:28:13 +0530

    Update Sumit.cpp
    9i

--- Sumit.cpp
+++ Sumit.cpp
@@ -4,12 +4,14 @@ using namespace std;
 #define ll long long int
 #define pb push_back//__standard_template_
 #define come_on ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0); //____program booster____
+
+#define m_p make_pair
+#define mod 1000000007
+
 #define pop pop_back
 #define str string
 #define endl "\n"
 #define vec vector
-#define m_p make_pair
-#define mod 1000000007
 main()
 {
     ll n,i,j,k;
