a32ce1f0e51617d55be65ef6cb902f6d2732ad88
bablucs12 <43750509+bablucs12@users.noreply.github.com>
2020-10-17 14:31:38 +0530

    Update Mcoins.cpp
    j0

--- Mcoins.cpp
+++ Mcoins.cpp
@@ -8,19 +8,21 @@ int arr[MAX];
 
 int main()
 {
-    l k,l,m,i;
+    ll k,l,m,i;
     cin>>k>>l>>m;
+  
     ll arr1[m];
     for( i=0;i<m;i++)
         cin>>arr1[i];
     arr[0]=0;
     for( i=1;i<MAX;i++)
     {
-        if(i-1>=0 && arr[i-1]==0)
-            arr[i]=1;
         if(i-k>=0 && arr[i-k]==0)
             arr[i]=1;
         if(i-l>=0 && arr[i-l]==0)
+            arr[i]=1;
+        
+        if(i-1>=0 && arr[i-1]==0)
             arr[i]=1;
     }
     
@@ -28,6 +30,7 @@ int main()
     {
         if(arr[arr1[i]]==1)
             cout<<"A";
+        
         else
             cout<<"B";
     }
